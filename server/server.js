be432d1ee553ce51549d218c0b3a4f3a4d47bdeb
madebygeelou <125709909+madebygeelou@users.noreply.github.com>
2024-05-28 21:21:21 -0700

    final touches of the night

--- server/server.js
+++ server/server.js
@@ -3,9 +3,39 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const app = express()
 const commentsRouter = require('./routes/comments')
-const path = require('path')
+const connectDB = require('./db');
+const fetchAndSaveVideos = require('./server/models/youtubeData');
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Connect to MongoDB
+connectDB();
+
+fetchAndSaveVideos('');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// New route to handle search queries
+app.post('/search', async (req, res) => {
+  const { query } = req.body;
+
+  if (!query) {
+    return res.status(400).json({ message: 'Search query is required' });
+  }
+
+  try {
+    await fetchAndSaveVideos(query);
+    res.status(200).json({ message: 'Videos fetched and saved successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching or saving videos', error: error.message });
+  }
+});
+
+//
+
 
 require('dotenv').config()
 const PORT = process.env.PORT || 3000;
@@ -17,12 +47,8 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     console.log('connected to mongo on: ', process.env.MONGO_URI)
 })
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build/index.html'));
-  });
-
 app.get('/', (req, res) => {
-    res.send('eat a dick')
+    res.send('')
 })
 
 app.listen(PORT, () => {
